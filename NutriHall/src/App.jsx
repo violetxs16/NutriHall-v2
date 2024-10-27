e03adf3aba871a147fb96fa825aee4c9ec032b64
violeta <violeta.solorio16@gmail.com>
2024-10-27 16:24:04 -0700

    Added new visuals for the front end

--- NutriHall/src/App.jsx
+++ NutriHall/src/App.jsx
@@ -1,35 +1,86 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx, Global } from "@emotion/react";
+import { useState } from "react";
 
+import MenuItems from "./Components/MenuItems";
+import MenuData from "./Components/MenuData";
+import Navbar from "./Components/Navbar";
 function App() {
-  const [count, setCount] = useState(0)
+  const [all, setAll] = useState(true);
+  const [breakfast, setBreakfast] = useState(false);
+  const [lunch, setLunch] = useState(false);
+  const [shakes, setShakes] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="App"
+      css={css`
+        background: #f0eff1;
+        height: 100%;
+        padding: 70px 0;
+      `}
+    >
+      <Navbar
+        setAll={setAll}
+        setBreakfast={setBreakfast}
+        setLunch={setLunch}
+        setShakes={setShakes}
+      />
+
+      <MenuItems
+        items={MenuData}
+        all={all}
+        breakfast={breakfast}
+        lunch={lunch}
+        shakes={shakes}
+      />
+
+      <Global
+        styles={css`
+          @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap");
+
+          ::selection {
+            background: #000;
+            color: #f0eff1;
+          }
+
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
+            --webkit-tap-highlight-color: transparent;
+          }
+
+          body::-webkit-scrollbar {
+            width: 12px; /* width of the entire scrollbar */
+          }
+
+          body::-webkit-scrollbar-track {
+            background: #f0eff1; /* color of the tracking area */
+          }
+
+          body::-webkit-scrollbar-thumb {
+            background-color: #444444; /* color of the scroll thumb */
+            border-radius: 20px; /* roundness of the scroll thumb */
+            border: 3px solid #f0eff1; /* creates padding around scroll thumb */
+          }
+
+          body {
+            background: #f0eff1;
+          }
+
+          .container {
+            width: 80%;
+            margin: auto;
+          }
+        `}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
