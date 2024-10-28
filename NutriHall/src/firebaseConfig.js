7e6ed134e65250a95f08270130c31b1671d731f2
violeta <violeta.solorio16@gmail.com>
2024-10-27 20:09:05 -0700

    Added firebaseConfig.js file to source folder

---
+++ NutriHall/src/firebaseConfig.js
@@ -0,0 +1,20 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; // Add other services you need
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDg1AXQ-uy1YSMH9cs8_KPutnls1efHEwQ",
+    authDomain: "nutrihall-26949.firebaseapp.com",
+    databaseURL: "https://nutrihall-26949-default-rtdb.firebaseio.com",
+    projectId: "nutrihall-26949",
+    storageBucket: "nutrihall-26949.appspot.com",
+    messagingSenderId: "561607807866",
+    appId: "1:561607807866:web:f0d779706806f698e6fcc3",
+    measurementId: "G-V8C0DHE8Y3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
