87c47206eb621c496177040a357ecbe9cad493e1
Amish Minocha <amish@Amishs-MacBook-Air.local>
2024-12-03 01:06:17 -0800

    see last commit

--- NutriHall/src/components/LP_Glance.jsx
+++ NutriHall/src/components/LP_Glance.jsx
@@ -6,9 +6,9 @@ const LP_Glance = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-        Taste, Track, and Transform{" "}
+        Taste, Track, and{" "}
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-           - NutriHall.
+           Transform
         </span>
       </h2>
       <div className="flex flex-wrap justify-center">
